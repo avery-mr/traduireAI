6aa2b4b04f6ba520032d277ed55cca9b6a162e7f
Mitchell Avery <averymr@colorado.edu>
2026-04-17 20:10:16 -0600

    rename baseline and transformer modules

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("traduireAI setup working")
+from datasets import load_dataset
+import baseline_model, transformer_model
+
+
+dataset = load_dataset(
+    "parquet",
+    data_files="../data/dev/en-it_dev.parquet"
+)
+
+print("dataset")
+print(dataset)
+print(dataset["test"][10]["src_text"])
+# nltk.download("punkt")
+# nltk.download("punkt_tab")
